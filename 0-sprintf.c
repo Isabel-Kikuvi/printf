a288e64c442d34211b9126fc1f6094028922083f
MarB-T <danmarbet@gmail.com>
2023-03-26 22:26:46 +0300

    Task 0, handles "s" and "c" format specifiers

---
+++ 0-sprintf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _printf - handles 'c' and 's' format specifiers
+ * @format: string of characters
+ * Return: number of 'prints'
+ */
+
+int _printf(const char *format, ...)
+{
+	int count;
+
+
+
+	return (count);
+}
