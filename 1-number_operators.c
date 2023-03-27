b78bd01f0076542c4933db2830648b26783e8689
Isabel-Kikuvi <isabel.kikuvi@gmail.com>
2023-03-27 09:22:05 -0700

    Task 3 - u o x X

--- 1-number_operators.c
+++ 1-number_operators.c
@@ -2,24 +2,51 @@
 
 /**
  * print_d - prints base 10 digits
- * @ar_list: stores the value of the arguments
+ * @list: stores the value of the arguments
  * Return: count print numbers
  */
 
-int print_d(va_list ar_list)
+int print_d(va_list list)
 {
+	unsigned int num_Abs, aux_Num, count_Zero, count;
+	int numbers;
 
+	count = 0;
 
+	numbers = va_arg(list, int);
+
+	if (numbers < 0)
+	{
+		num_Abs = (numbers * -1);
+		count += _putchar(45);
+	}
+	else
+		num_Abs = numbers;
+
+	aux_Num = num_Abs;
+	count_Zero = 1;
+	while (aux_Num > 9)
+	{
+		aux_Num /= 10;
+		count_Zero *= 10;
+	}
+
+	while (count_Zero >= 1)
+	{
+		count += _putchar(((num_Abs / count_Zero) % 10) + '0');
+		count_Zero /= 10;
+	}
 	return (count);
+
 }
 
 /**
  * print_i - prints integers
- * @ar_list: stores list of numbers
+ * @list: stores list of numbers
  * Return: Number print
  */
 
-int print_i(va_list ar_list)
+int print_i(va_list list)
 {
-	return (print_d(ar_list));
+	return (print_d(list));
 }
--- main.h
+++ main.h
@@ -4,7 +4,11 @@
 int _putchar(char c);
 int _printf(const char *format, ...);
 void print_binary(unsigned int, num);
-int print_d(va_list ar_list);
-int print_i(va_list ar_list);
+int print_d(va_list list);
+int print_i(va_list list);
+int print_unsigned(va_list list);
+int print_octal(va_list list);
+int print_hex(va_list list);
+int print_HEX(va_list list);
 
 #endif
